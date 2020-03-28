e7819c9451d4917148ead065a423f023b29ca413
Moses Ogwo <mosesogwo@gmail.com>
2020-03-28 16:13:51 +0100

    add Question spec and validations

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # Validations
+  it { should validate_presence_of(:q_text) }
+  it { should validate_presence_of(:q_options) }
+  it { should validate_presence_of(:q_answer) }
+  it { should validate_length_of(:q_answer).is_equal_to(1) }
+
+  # Associations
+  it { should belong_to(:test) }
 end
