8c7af1622ac7c042ac09a767f4aeb6d6a82d6569
Moses Ogwo <mosesogwo@gmail.com>
2020-04-14 15:43:05 +0100

    add users controller create method

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,3 +1,30 @@
-class Api::V1::UsersController < ApplicationController
-  
+module Api
+  module V1
+    class UsersController < ApplicationController
+
+      def create
+        user = User.new(user_params)
+        if user.save
+          render json: { status: 'SUCCESS', message: 'Account Successfully Created', data: user }, status: :ok
+        else
+          render json: { status: 'ERROR', message: 'Something went wrong, account creation failed', data: user.errors }, status: :unprocessable_entity
+        end
+      end
+
+      def edit
+        # user = User.find_by(id: )
+      end
+
+      def destroy
+      end
+
+
+      private
+
+      def user_params
+        params.permit(:name, :email, :password)
+      end
+
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       post 'authenticate', to: 'authentication#authenticate'
+      resources :users, only: [:create, :edit, :destroy]
     end
   end
 end
