9d01e6215c691d7be664548ede00fb6b5359a003
Moses Ogwo <mosesogwo@gmail.com>
2020-05-04 08:21:58 +0100

    add uniqueness verification for result

--- app/models/result.rb
+++ app/models/result.rb
@@ -1,6 +1,6 @@
 class Result < ApplicationRecord
   validates :score, :remark, presence: true
-  validates :test, uniqueness: { scope: :user, message: 'One result per user per test' }
+  validates :test, uniqueness: { scope: :candidate, message: 'One result per user per test' }
   belongs_to :test
   belongs_to :candidate, class_name: 'User'
 end
